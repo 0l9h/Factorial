a47f9374e3798872773758dd2d9113739ba3190b
Oleh <olehbobeliak@gmail.com>
2021-06-27 12:21:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(factorial(20));
+        }
+
+        static long factorial(long val)
+        {
+            if (val > 1)
+            {
+                return val*factorial(val - 1);
+            }
+            return val;
+        }
+    }
+}
